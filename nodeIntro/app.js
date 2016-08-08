6a1ef0a6bbf71b22208d9645e345963fadb174aa
Dawson Greeley <dawsonmax10@gmail.com>
2016-08-08 16:31:44 +0000

    Added app.js

---
+++ nodeIntro/app.js
@@ -0,0 +1,12 @@
+// var a = require('knock-knock-jokes');
+
+// console.log(a());
+
+function echo(word, amt){
+    for(var i = 0; i < amt; i++){
+        console.log(word);
+    }
+}
+
+echo("echo!!", 10);
+echo("Tater tots", 3);
